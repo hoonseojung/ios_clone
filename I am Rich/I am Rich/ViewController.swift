7e5eb5dd227dd3298368b711b8bedb6bad0cc99d
Hoon_Seo <80146349+hoonseojung@users.noreply.github.com>
2021-05-26 02:43:51 +0900

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,14 @@
+//  I am Rich
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
