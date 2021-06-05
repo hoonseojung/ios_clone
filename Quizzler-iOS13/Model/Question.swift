b75408dc234b5a0ca0707384128e1bca45cffe22
Hoon_Seo <80146349+hoonseojung@users.noreply.github.com>
2021-06-06 00:14:21 +0900

    Update Question.swift

--- Quizzler-iOS13/Model/Question.swift
+++ Quizzler-iOS13/Model/Question.swift
@@ -1,11 +1,3 @@
-//
-//  Question.swift
-//  Quizzler-iOS13
-//
-//  Created by 정훈서 on 2021/05/25.
-//  Copyright © 2021 The App Brewery. All rights reserved.
-//
-
 import Foundation
 
 struct Question {
