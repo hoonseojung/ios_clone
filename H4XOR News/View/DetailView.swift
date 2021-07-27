1c345b74ea0d9d932550cac5f386eb695dbf664b
Hoon_Seo <80146349+hoonseojung@users.noreply.github.com>
2021-07-27 20:04:44 +0900

    Add files via upload

---
+++ H4XOR News/View/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  H4XOR News
+//
+//  Created by 정훈서 on 2021/07/25.
+//
+
+import SwiftUI
+import WebKit
+
+struct DetailView: View {
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
